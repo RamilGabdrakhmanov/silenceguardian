9a15b4c64b2cf1aa9bb2c637e43ab313999f99e7
Ramil Gabdrakhmanov <ramil.gabdrakhmanov@flatstack.com>
2016-11-24 13:17:08 +0300

    Add google api; show notification entering/exiting in/from geofence;

--- app/src/main/java/fs/silenceguardian/MainActivity.java
+++ app/src/main/java/fs/silenceguardian/MainActivity.java
@@ -1,13 +1,160 @@
 package fs.silenceguardian;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, ResultCallback<Status> {
+
+    protected static final String TAG = "MainActivity";
+
+    protected GoogleApiClient     mGoogleApiClient;
+    protected ArrayList<Geofence> mGeofenceList;
+    private   PendingIntent       mGeofencePendingIntent;
+    private   SharedPreferences   mSharedPreferences;
+    private   boolean             mGeofencesAdded;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSharedPreferences = getSharedPreferences("flatstack", MODE_PRIVATE);
+        mGeofenceList = new ArrayList<>();
+        mGeofencePendingIntent = null;
+
+
+        mGeofencesAdded = mSharedPreferences.getBoolean("GeofencesAdded", false);
+        populateGeofenceList();
+        buildGoogleApiClient();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+            .addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
+            .addApi(LocationServices.API)
+            .build();
+    }
+
+    private GeofencingRequest getGeofencingRequest() {
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+
+        mGeofencePendingIntent = null;
+
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
+
+        builder.addGeofences(mGeofenceList);
+
+        return builder.build();
+    }
+
+    public void AddGeofencing() {
+        if (!mGoogleApiClient.isConnected()) {
+            Toast.makeText(this, getString(R.string.not_connected), Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        try {
+            LocationServices.GeofencingApi.addGeofences(
+                mGoogleApiClient,
+                getGeofencingRequest(),
+                getGeofencePendingIntent()
+            ).setResultCallback(this);
+        } catch (SecurityException securityException) {
+            logSecurityException(securityException);
+        }
+    }
+
+    private void logSecurityException(SecurityException securityException) {
+        Log.e(TAG, "Invalid location permission. " +
+            "You need to use ACCESS_FINE_LOCATION with geofences", securityException);
+    }
+
+    @Override public void onConnected(@Nullable Bundle bundle) {
+        Log.i(TAG, "Connected to GoogleApiClient");
+        if (!mGeofencesAdded) {
+            AddGeofencing();
+        }
+    }
+
+    @Override public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Connection suspended");
+    }
+
+    @Override public void onConnectionFailed(@NonNull ConnectionResult result) {
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " +
+            result.getErrorCode() + " getErrorMessage = "+ result.getErrorMessage());
+    }
+
+    private PendingIntent getGeofencePendingIntent() {
+        if (mGeofencePendingIntent != null) {
+            return mGeofencePendingIntent;
+        }
+        Intent intent = new Intent(this, GeofenceTransitionsIntentService.class);
+        return PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    public void populateGeofenceList() {
+
+        mGeofenceList.add(new Geofence.Builder()
+            .setRequestId("Flatstack House")
+            .setCircularRegion(
+                55.793764, 49.125353, 10
+            )
+            .setExpirationDuration(Geofence.NEVER_EXPIRE)
+            .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
+                Geofence.GEOFENCE_TRANSITION_EXIT)
+            .build());
+
+    }
+
+    @Override
+    public void onResult(Status status) {
+        if (status.isSuccess()) {
+            mGeofencesAdded = !mGeofencesAdded;
+            SharedPreferences.Editor editor = mSharedPreferences.edit();
+            editor.putBoolean("GeofencesAdded", mGeofencesAdded);
+            editor.apply();
+
+            Toast.makeText(
+                this,
+                getString(mGeofencesAdded ? R.string.geofences_added :
+                    R.string.geofences_removed),
+                Toast.LENGTH_SHORT
+            ).show();
+        } else {
+            String errorMessage = GeofenceErrorMessages.getErrorString(this,
+                status.getStatusCode());
+            Log.e(TAG, errorMessage);
+        }
     }
 }
